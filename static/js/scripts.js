49110b7eb50abeceb2ee97cdba81287106b7c97f
caslee25 <ceasylee@gmail.com>
2024-05-31 09:49:39 -0400

    functional for ipad

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -69,132 +69,147 @@ window.onload = function() {
 };
 
 document.addEventListener('DOMContentLoaded', function() {
-  const calendarDays = document.getElementById('calendarDays');
-  const currentWeek = document.getElementById('currentWeek');
-  const prevWeekButton = document.getElementById('prevWeek');
-  const nextWeekButton = document.getElementById('nextWeek');
-
-  let currentDate = new Date();
-
-  function renderWeek(date) {
+    const calendarDays = document.getElementById('calendarDays');
+    const currentWeek = document.getElementById('currentWeek');
+    const prevWeekButton = document.getElementById('prevWeek');
+    const nextWeekButton = document.getElementById('nextWeek');
+  
+    let currentDate = new Date();
+  
+    function renderWeek(date) {
       calendarDays.innerHTML = '';
       const startOfWeek = getStartOfWeek(date);
       const endOfWeek = new Date(startOfWeek);
       endOfWeek.setDate(endOfWeek.getDate() + 6);
-
+  
       currentWeek.textContent = `${formatDate(startOfWeek)} - ${formatDate(endOfWeek)}`;
-
+  
       for (let i = 0; i < 7; i++) {
-          const dayCell = document.createElement('div');
-          dayCell.classList.add('day');
-          const currentDay = new Date(startOfWeek);
-          currentDay.setDate(currentDay.getDate() + i);
-          dayCell.textContent = currentDay.getDate();
-
-          // Add events for the current day
-          events.forEach(event => {
-            const eventDate = new Date(event.start_date);
-            const adjustedDay = new Date(currentDay);
-            adjustedDay.setDate(adjustedDay.getDate() - 1);
-            if (eventDate.toDateString() === adjustedDay.toDateString()) {
-                const eventDiv = document.createElement('div');
-                eventDiv.classList.add('event');
-
-                const titleDiv = document.createElement('div');
-                titleDiv.classList.add('event-title');
-                titleDiv.textContent = event.title;
-
-                const nameDiv = document.createElement('div');
-                nameDiv.classList.add('event-name');
-                nameDiv.textContent = event.name;
-
-                const descriptionDiv = document.createElement('div');
-                descriptionDiv.classList.add('event-description');
-                descriptionDiv.textContent = event.description;
-
-                const startTime12Hour = convertTo12Hour(event.start_time);
-                const timeDiv = document.createElement('div');
-                timeDiv.classList.add('event-time');
-                timeDiv.textContent = "Starts at " + startTime12Hour;
-
-
-                eventDiv.appendChild(titleDiv);
-                eventDiv.appendChild(nameDiv);
-                eventDiv.appendChild(descriptionDiv);
-                eventDiv.appendChild(timeDiv);
-                
-                
-                if (event.end_time){
-                    const startTime12Hour = convertTo12Hour(event.end_time);
-                    const end_timeDiv = document.createElement('div');
-                    end_timeDiv.classList.add('event-time');
-                    end_timeDiv.textContent = "Ends at " + startTime12Hour;
-                    eventDiv.appendChild(end_timeDiv);
-                }
-
-                if (event.category){
-                    const capitalizedCategory = event.category.split(' ')
-                        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-                        .join(', ');
-                    const tagsDiv = document.createElement('div');
-                    tagsDiv.classList.add('event-tags');
-                    tagsDiv.textContent = "Tags : " + capitalizedCategory;
-                    eventDiv.appendChild(tagsDiv);
-                }
-                dayCell.appendChild(eventDiv);
+        const dayCell = document.createElement('div');
+        dayCell.classList.add('day');
+        const currentDay = new Date(startOfWeek);
+        currentDay.setDate(currentDay.getDate() + i);
+  
+        const dayOfWeek = currentDay.toLocaleDateString('en-US', { weekday: 'short' }).slice(0, 3);
+        const dayOfWeekElement = document.createElement('div');
+        dayOfWeekElement.textContent = dayOfWeek;
+  
+        const dayDateElement = document.createElement('div');
+        dayDateElement.textContent = currentDay.getDate();
+  
+        dayCell.appendChild(dayOfWeekElement);
+        dayCell.appendChild(dayDateElement);
+  
+        // Highlight the current day
+        const today = new Date();
+        if (currentDay.toDateString() === today.toDateString()) {
+          dayCell.classList.add('current-day');
+          dayOfWeekElement.classList.add('current-day-of-week');
+        }
+  
+        // Add events for the current day
+        events.forEach(event => {
+          const eventDate = new Date(event.start_date);
+          const adjustedDay = new Date(currentDay);
+          adjustedDay.setDate(adjustedDay.getDate() - 1);
+          if (eventDate.toDateString() === adjustedDay.toDateString()) {
+            const eventDiv = document.createElement('div');
+            eventDiv.classList.add('event');
+  
+            const titleDiv = document.createElement('div');
+            titleDiv.classList.add('event-title');
+            titleDiv.textContent = event.title;
+  
+            const nameDiv = document.createElement('div');
+            nameDiv.classList.add('event-name');
+            nameDiv.textContent = event.name;
+  
+            const descriptionDiv = document.createElement('div');
+            descriptionDiv.classList.add('event-description');
+            descriptionDiv.textContent = event.description;
+  
+            const startTime12Hour = convertTo12Hour(event.start_time);
+            const timeDiv = document.createElement('div');
+            timeDiv.classList.add('event-time');
+            timeDiv.textContent = "Starts at " + startTime12Hour;
+  
+            eventDiv.appendChild(titleDiv);
+            eventDiv.appendChild(nameDiv);
+            eventDiv.appendChild(descriptionDiv);
+            eventDiv.appendChild(timeDiv);
+  
+            if (event.end_time) {
+              const endTime12Hour = convertTo12Hour(event.end_time);
+              const endTimeDiv = document.createElement('div');
+              endTimeDiv.classList.add('event-time');
+              endTimeDiv.textContent = "Ends at " + endTime12Hour;
+              eventDiv.appendChild(endTimeDiv);
             }
+  
+            if (event.category) {
+              const capitalizedCategory = event.category.split(' ')
+                .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+                .join(', ');
+              const tagsDiv = document.createElement('div');
+              tagsDiv.classList.add('event-tags');
+              tagsDiv.textContent = "Tags : " + capitalizedCategory;
+              eventDiv.appendChild(tagsDiv);
+            }
+            dayCell.appendChild(eventDiv);
+          }
         });
-
-          calendarDays.appendChild(dayCell);
+  
+        calendarDays.appendChild(dayCell);
       }
-  }
-
-  function convertTo12Hour(time) {
-    const [hours, minutes] = time.split(':');
-    let period = 'AM';
-    let hour = parseInt(hours);
-
-    if (hour >= 12) {
+    }
+  
+    function convertTo12Hour(time) {
+      const [hours, minutes] = time.split(':');
+      let period = 'AM';
+      let hour = parseInt(hours);
+  
+      if (hour >= 12) {
         period = 'PM';
         if (hour > 12) {
-            hour -= 12;
+          hour -= 12;
         }
-    }
-    if (hour === 0) {
+      }
+      if (hour === 0) {
         hour = 12;
+      }
+  
+      return `${hour}:${minutes} ${period}`;
     }
-
-    return `${hour}:${minutes} ${period}`;
-}
-
-  function getStartOfWeek(date) {
+  
+    function getStartOfWeek(date) {
       const day = date.getDay();
       const diff = date.getDate() - day;
       const startOfWeek = new Date(date);
       startOfWeek.setDate(diff);
       return startOfWeek;
-  }
-
-  function formatDate(date) {
+    }
+  
+    function formatDate(date) {
       return date.toLocaleDateString('en-US', {
-          month: 'short',
-          day: 'numeric',
-          year: 'numeric'
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric'
       });
-  }
-
-  function changeWeek(offset) {
+    }
+  
+    function changeWeek(offset) {
       currentDate.setDate(currentDate.getDate() + offset * 7);
       renderWeek(currentDate);
-  }
-
-  prevWeekButton.addEventListener('click', function() {
+    }
+  
+    prevWeekButton.addEventListener('click', function() {
       changeWeek(-1);
-  });
-
-  nextWeekButton.addEventListener('click', function() {
+    });
+  
+    nextWeekButton.addEventListener('click', function() {
       changeWeek(1);
+    });
+  
+    renderWeek(currentDate);
   });
-
-  renderWeek(currentDate);
-});
+  
